9af6502ceddb4714d64814c35d8e75c7c4b4f482
nadav.meidan <nadav.medan@mail.huji.ac.il>
2020-01-06 16:05:26 +0200

    i've created the header and C files

---
+++ myset.c
@@ -0,0 +1,5 @@
+//
+// Created by nadav.meidan on 06/01/2020.
+//
+
+#include "myset.h"
---
+++ myset.h
@@ -0,0 +1,8 @@
+//
+// Created by nadav.meidan on 06/01/2020.
+//
+
+#ifndef MAABADA_MMN22_MYSET_H
+#define MAABADA_MMN22_MYSET_H
+
+#endif //MAABADA_MMN22_MYSET_H
