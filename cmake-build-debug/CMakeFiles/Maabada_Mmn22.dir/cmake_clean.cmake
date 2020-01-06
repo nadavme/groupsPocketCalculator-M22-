506d3118dd300e63bb8af650a3981268e6b4617a
nadav.meidan <nadav.medan@mail.huji.ac.il>
2020-01-06 15:56:41 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Maabada_Mmn22.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Maabada_Mmn22.dir/main.c.o"
+  "Maabada_Mmn22.pdb"
+  "Maabada_Mmn22"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Maabada_Mmn22.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
