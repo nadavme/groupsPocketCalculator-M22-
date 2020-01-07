9d9089b2d2d5ea7d7efc2656af73866225ee24f8
Nadav Meidan <nadav.medan@mail.huji.ac.il>
2020-01-07 16:02:55 +0200

    set.c updates

--- set.c
+++ set.c
@@ -3,3 +3,38 @@
 //
 
 #include "set.h"
+
+void read_set(char[] input, args)
+{
+
+}
+
+void print_set(set set)
+{
+
+}
+
+void union_set(set a, set b, set c);
+{
+
+}
+
+void intersect_set(set a, set b, set c);
+{
+
+}
+
+void sub_set(set a, set b, set c);
+{
+
+}
+
+void symdiff_set(set a, set b, set c)
+{
+
+}
+
+void stop()
+{
+
+}
