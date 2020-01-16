b21fd89466f27271b48ecb38de316af7806cf779
Nadav Meidan <nadav.medan@mail.huji.ac.il>
2020-01-16 11:10:42 +0200

    trying to make some new main2

---
+++ main.c
@@ -0,0 +1,21 @@
+//
+// Created by nadav on 15/01/2020.
+//
+
+#include "main.h"
+
+void excuteCommand()
+{
+    void* command;
+    parseCommand();
+}
+
+
+void parseCommand()
+{
+
+}
+int main()
+{
+
+}
---
+++ main.h
@@ -0,0 +1,21 @@
+//
+// Created by nadav on 15/01/2020.
+//
+
+#ifndef MAABADA_MMN22_MAIN_H
+#define MAABADA_MMN22_MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+#include "stdlib.h"
+#include "set.h"
+
+enum StringsOfSets {SETA, SETB, SETC, SETD, SETE, SETF};
+
+void excuteCommand();
+
+void parseCommand();
+
+
+
+#endif //MAABADA_MMN22_MAIN_H
